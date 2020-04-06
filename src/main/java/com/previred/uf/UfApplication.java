d6fb756c6a0fbc56ee9f403af6b3725c2c99dce3
Alejandro <alejandrovs88@gmail.com>
2020-04-06 16:32:04 -0400

    Init Maven project

---
+++ src/main/java/com/previred/uf/UfApplication.java
@@ -0,0 +1,16 @@
+package com.previred.uf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfApplication {
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				// your logic here
+			}
+		});
+	}
+}
---
+++ src/test/java/com/previred/uf/UfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.previred.uf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
