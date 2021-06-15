36e211cbd8015f9ec0c78ffa220a81ca60a1b548
chago <chago022895@gmail.com>
2021-06-15 11:43:36 -0400

    initial commit

---
+++ src/main/java/com/desafio/tres/valoresuf/ValoresufApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.tres.valoresuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValoresufApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValoresufApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/desafio/tres/valoresuf/ValoresufApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.tres.valoresuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValoresufApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
